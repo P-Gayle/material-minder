4ee62badf8516425c2e38b74c4f486760f97b729
Paula Gayle <paula_gayle@hotmail.com>
2023-06-10 23:05:46 +0100

    Add an item to the database successfully

--- api/index.php
+++ api/index.php
@@ -4,4 +4,35 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-headers: *");
 
-echo "Testing";
+include 'DbConnect.php';
+
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case "POST":
+        $item = json_decode(file_get_contents('php://input'));
+        $sql =
+        "INSERT INTO supplies(id, name, type, quantity, location, colour, supplier, created_at)
+        VALUES(null, :name, :type, :quantity, :location, :colour, :supplier, :created_at)";
+        $stmt = $conn->prepare($sql);
+        $created_at = date('Y-m-d');
+        //the below statements won't work unless json_decode $item above
+        $stmt->bindparam(':name', $item->name);
+        $stmt->bindparam(':type', $item->type);
+        $stmt->bindparam(':quantity', $item->quantity);
+        $stmt->bindparam(':location', $item->location);
+        $stmt->bindparam(':colour', $item->colour);
+        $stmt->bindparam(':supplier', $item->supplier);
+        $stmt->bindparam(':created_at', $created_at);
+
+        if($stmt->execute()){
+            $response = ['status' => 1, 'message' => 'Record created successfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record'];
+        }
+        echo json_encode($response);
+        break;
+}
